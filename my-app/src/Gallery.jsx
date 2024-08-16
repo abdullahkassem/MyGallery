e1b20cea15825b938c0273a021ca953f7d4f81bc
Abdullah Kassem <abdullah.kassem.c@propertyfinder.ae>
2024-08-16 15:31:42 +0400

    Scrolling support added however you cannot use both scrolling and nav buttons or dots

--- my-app/src/Gallery.jsx
+++ my-app/src/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import SwitchImgButton from './SwitchImgButton'
 import './styles/gallery.scss';
 import GalleryImg from './GalleryImg';
@@ -11,17 +11,43 @@ export default function Gallery({ imgDirArr }) {
   const [curImgIdx, setcurImgIdx] = useState(0); // first to be shown
   console.log("curImgIndex:",curImgIdx);
 
+  const carouselRef = useRef(null);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (carouselRef.current) {
+        const { scrollLeft, offsetWidth } = carouselRef.current;
+        console.log({ scrollLeft, offsetWidth });
+        const index = Math.floor(scrollLeft / (offsetWidth-20));
+        setcurImgIdx(index);
+        console.log("scrolling has happened by ",index," images");
+      }
+    };
   
+    const carousel = carouselRef.current; // dom element 
+    carousel.addEventListener('scroll', handleScroll);
+  
+    // Clean up function
+    return () => {
+      if (carousel) {
+        carousel.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
+
   return (
     <div className='galleryContainer' >
       <SwitchImgButton direction={"backwards"} curImgIdx={curImgIdx} setcurImgIdx={setcurImgIdx} imagesLength={imgDirArr.length} />
 
-      <div className="imageContainer">
+      <div ref={carouselRef} className="imageContainer">
         {imgDirArr.map((image, index) => {
           return (
             <GalleryImg key={index} img={image} />
           )
         })}
+
       </div>
 
       <SwitchImgButton direction={"forwards"} curImgIdx={curImgIdx} setcurImgIdx={setcurImgIdx} imagesLength={imgDirArr.length} />
